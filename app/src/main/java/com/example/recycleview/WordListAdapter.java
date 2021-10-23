fa32a2ac5ecb850b1487123ed717bf14c6fd8776
POSTE2 <Mustapha El Masri>
2021-10-23 10:20:58 +0100

    hello

---
+++ app/src/main/java/com/example/recycleview/WordListAdapter.java
@@ -0,0 +1,63 @@
+package com.example.recycleview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
+
+public class WordListAdapter extends
+        RecyclerView.Adapter<WordListAdapter.WordViewHolder>  {
+    class WordViewHolder extends RecyclerView.ViewHolder {
+
+
+    public final TextView wordItemView;
+    final WordListAdapter mAdapter;
+
+
+    public WordViewHolder(View itemView, WordListAdapter adapter) {
+        super(itemView);
+        wordItemView = itemView.findViewById(R.id.word);
+        this.mAdapter = adapter;
+    }}
+    private LayoutInflater mInflater;
+    private final LinkedList<String> mWordList;
+
+    public WordListAdapter(Context context,
+                           LinkedList<String> wordList) {
+        mInflater = LayoutInflater.from(context);
+        this.mWordList = wordList;
+    }
+
+
+    @NonNull
+    @Override
+    public WordListAdapter.WordViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View mItemView = mInflater.inflate(R.layout.wordlist_item,
+                parent, false);
+        return new WordViewHolder(mItemView, this);
+    }
+
+
+
+    @Override
+    public void onBindViewHolder(WordViewHolder holder, int position) {
+        String mCurrent = mWordList.get(position);
+        holder.wordItemView.setText(mCurrent);
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mWordList.size();
+    }
+
+
+
+}
